f39dcb85786c55b521f146d9bbcc04a0c2c23e5f
Nicolas Fumo <49803280+NicolasFumo@users.noreply.github.com>
2023-01-20 21:04:06 -0300

    Create Clase 20-01.cs

---
+++ Codigo clases/Clase 20-01.cs
@@ -0,0 +1,34 @@
+//nombre   ( parametro) -> dato a mostrar en pantalla
+Console.WriteLine(70/5);
+
+// Tipos de datos
+// -> " " texto -> string
+// -> numero -> entero -> int
+
+//------------------------
+
+// Variables
+// Crear la variable -> declarar / guardar datos / leer datos
+
+// como se declara una variable -> tipo nombre;
+
+string nombrePersona; // declaracion
+
+nombrePersona = "Nicolas"; // guardamos un dato
+
+Console.WriteLine(nombrePersona); // leer un dato
+
+nombrePersona = "Micaela"; // Cambio el dato de la variable
+
+Console.WriteLine(nombrePersona); // vuelvo a mostrar la variable
+
+
+int Edad;
+
+Edad = 21;
+
+Console.WriteLine(Edad);
+
+Console.WriteLine(nombrePersona);
+
+Console.WriteLine();
